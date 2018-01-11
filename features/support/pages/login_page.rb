5a95048e8c246d402a9703dd8e34828f0d17bd13
Nenad Tanaskovic <nenad@fintechdev.net>
2018-01-11 22:03:12 +0100

    FD-4720: Change URL to FT1

--- features/support/pages/login_page.rb
+++ features/support/pages/login_page.rb
@@ -1,7 +1,7 @@
 class LoginPage
   include PageObject
 
-  page_url "https://rpm.gateway.payeezyqa.com/?lang=en"
+  page_url "https://admin-ft1.gateway.payeezytest.com/?lang=en"
 
   text_field(:login_name, id: "login_name")
   text_field(:password, id: "login_password")
